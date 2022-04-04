6dd44de5a853fbcf6c17630171db606f98d4e00e
Matt Wilson <mattwwilson34@gmail.com>
2022-04-04 08:54:11 -0400

    Added test middleware

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+// a middleware function with no mount path. This code is executed for every request to the router
+router.use('/users/cool', (req, res, next) => {
+  console.log('Time:', Date.now());
+  next();
+});
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
