932aa567fe701d871e6bd583406b61d18b250e6f
Matt Wilson <mattwwilson34@gmail.com>
2022-04-07 20:34:22 -0400

    Updated routes

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-// a middleware function with no mount path. This code is executed for every request to the router
-router.use('/users/cool', (req, res, next) => {
-  console.log('Time:', Date.now());
-  next();
-});
-
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+// GET home page.
+router.get('/', function (req, res) {
+  res.redirect('/catalog');
 });
 
 module.exports = router;
